95fb0e2af62acc4b3ea4d88e1832325807467143
agamemnus <agamemnus@gmail.com>
2014-10-29 02:59:39 -0400

    Put initialization in a separate callable function.

--- www/googleplayservices.js
+++ www/googleplayservices.js
@@ -10,5 +10,11 @@ module.exports = function () {
   cordova.exec (success, error, "GooglePlayServices", "getPlayerId", [])
  }
  
+ exports.initialize = function (init) {
+  var success = (typeof init.success != "undefined") ? init.success : function () {}
+  var error   = (typeof init.error   != "undefined") ? init.error   : function () {}
+  cordova.exec (success, error, "GooglePlayServices", "initialize", [])
+ }
+ 
  return exports
 } ()
