7109ff50a2270d2484208bfeb0928bf511324846
agamemnus <agamemnus@gmail.com>
2014-11-03 19:10:52 -0500

    Update GooglePlayServices.java

--- src/android/GooglePlayServices.java
+++ src/android/GooglePlayServices.java
@@ -8,6 +8,12 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.games.Players;
 import com.google.android.gms.games.Games;
 
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.plus.Account;
+import com.google.android.gms.plus.Plus;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -22,21 +28,27 @@ import org.json.JSONObject;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
-//import android.app.Activity;
 
+import android.app.Activity;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import org.apache.cordova.*;
+
+import java.io.IOException;
 
 import android.util.Log;
 
 public class GooglePlayServices extends CordovaPlugin implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
  
  private static final String LOGTAG = "GooglePlayServices";
+ private static final int REQ_SIGN_IN_REQUIRED = 55664;
  
- private CordovaInterface cordova            = null;
- private CordovaWebView   webView            = null;
- private GoogleApiClient  mGoogleApiClient   = null;
- private CallbackContext  tryConnectCallback = null;
+ public CordovaInterface        cordova            = null;
+ public CordovaWebView          webView            = null;
+ public  static GoogleApiClient mGoogleApiClient   = null;
+ public CallbackContext         tryConnectCallback = null;
+ public String                  accessToken        = "";
  
  @Override public void initialize (CordovaInterface initCordova, CordovaWebView initWebView) {
   cordova  = initCordova;
@@ -47,7 +59,7 @@ public class GooglePlayServices extends CordovaPlugin implements GoogleApiClient
  @Override public void onConnectionFailed (ConnectionResult result) {
   if (!result.hasResolution()) {Log.w (LOGTAG, "Error: no resolution. Google Play Services connection failed."); return;}
   try {
-   result.startResolutionForResult (cordova.getActivity(), result.SIGN_IN_REQUIRED);
+   result.startResolutionForResult (cordova.getActivity(), result.getErrorCode());
   } catch (SendIntentException e) {
    // There was an error with the resolution intent. Try again.
    mGoogleApiClient.connect ();
@@ -55,17 +67,15 @@ public class GooglePlayServices extends CordovaPlugin implements GoogleApiClient
  }
  
  @Override public void onConnected (Bundle connectionHint) {
+  String mAccountName = Plus.AccountApi.getAccountName(mGoogleApiClient);
+  new RetrieveTokenTask().execute (mAccountName);
   Games.setViewForPopups (mGoogleApiClient, webView);
-  if (tryConnectCallback != null) {
-   String playerId = Games.Players.getCurrentPlayerId (mGoogleApiClient);
-   tryConnectCallback.sendPluginResult (new PluginResult (PluginResult.Status.OK, playerId));
-   tryConnectCallback = null;
-  }
  }
  
- @Override public void onActivityResult (int requestCode, int responseCode, Intent intent) {
+ public void onActivityResult (int requestCode, int responseCode, Intent intent) {
   if (!mGoogleApiClient.isConnecting()) mGoogleApiClient.connect ();
  }
+ 
  @Override public void onConnectionSuspended (int cause) {
   mGoogleApiClient.connect ();
  }
@@ -75,17 +85,17 @@ public class GooglePlayServices extends CordovaPlugin implements GoogleApiClient
    String playerId = Games.Players.getCurrentPlayerId (mGoogleApiClient);
    callbackContext.sendPluginResult (new PluginResult (PluginResult.Status.OK, playerId));
   } else if ("tryConnect".equals(action)) {
-   // Passes the callbackContext to tryConnect ().
-   // tryConnect runs the callback with a value of false if Google Play Services isn't available.
    tryConnect (callbackContext);
+  } else if ("getAccessToken".equals(action)) {
+   callbackContext.sendPluginResult (new PluginResult (PluginResult.Status.OK, accessToken));
   }
   return true;
  }
  
+ // tryConnect runs the callback with a value of false if Google Play Services isn't available.
  public void tryConnect (CallbackContext callbackContext) {
   boolean isGpsAvailable = (GooglePlayServicesUtil.isGooglePlayServicesAvailable(cordova.getActivity()) == ConnectionResult.SUCCESS);
   if (!isGpsAvailable) {
-   Log.w (LOGTAG, "Error: Google Play Services is not available.");
    callbackContext.sendPluginResult (new PluginResult (PluginResult.Status.OK, false));
    return;
   }
@@ -95,7 +105,40 @@ public class GooglePlayServices extends CordovaPlugin implements GoogleApiClient
    .addOnConnectionFailedListener (this)
    .addApi (Games.API)
    .addScope (Games.SCOPE_GAMES)
+   .addApi(Plus.API)
+   .addScope(Plus.SCOPE_PLUS_LOGIN)
    .build ();
   mGoogleApiClient.connect ();
+ }
+ 
+ 
+ private class RetrieveTokenTask extends AsyncTask<String, Void, String> {
+  @Override protected String doInBackground (String... params) {
+   String accountName = params[0];
+   String scopes = "oauth2:profile email";
+   String token = null;
+   Context context =  cordova.getActivity().getApplicationContext();
+    Log.e (LOGTAG, "RetrieveTokenTask");
+   try {
+    token = GoogleAuthUtil.getToken (context, accountName, scopes);
+   } catch (IOException e) {
+    Log.e (LOGTAG, e.getMessage());
+   } catch (UserRecoverableAuthException e) {
+    cordova.getActivity().startActivityForResult (e.getIntent(), REQ_SIGN_IN_REQUIRED);
+   } catch (GoogleAuthException e) {
+    Log.e (LOGTAG, e.getMessage());
+   }
+   return token;
+  }
+  
+  @Override protected void onPostExecute (String newAccessToken) {
+   accessToken = newAccessToken;   
+   if (tryConnectCallback != null) {
+    String playerId = Games.Players.getCurrentPlayerId (mGoogleApiClient);
+    tryConnectCallback.sendPluginResult (new PluginResult (PluginResult.Status.OK, playerId));
+    tryConnectCallback = null;
+   }
+   super.onPostExecute (newAccessToken);
+  }
  }
 }
