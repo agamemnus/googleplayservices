093412690513e13b32b55ef6c80767002ee2d275
agamemnus <agamemnus@gmail.com>
2014-11-03 19:11:09 -0500

    Update sample_activity_file.java

--- sample_activity_file.java
+++ sample_activity_file.java
@@ -3,18 +3,12 @@ package com.myapp;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
-import android.util.Log;
-
 import android.content.Intent;
 
 import com.flyingsoftgames.googleplayservices.GooglePlayServices;
 
 public class MyApp extends CordovaActivity {
-
- private static final String LOGTAG = "MyApp";
- 
  public void onActivityResult (int requestCode, int responseCode, Intent intent) {
-  Log.w (LOGTAG, "onActivityResult");
   if (!GooglePlayServices.mGoogleApiClient.isConnecting()) GooglePlayServices.mGoogleApiClient.connect ();
  }
  
