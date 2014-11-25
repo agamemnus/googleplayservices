234de1a1b80bfae0c2d31dd7c83da30864c845ed
agamemnus <agamemnus@gmail.com>
2014-11-25 14:11:33 -0500

    Update googleplayservices.js

--- www/googleplayservices.js
+++ www/googleplayservices.js
@@ -1,14 +1,8 @@
-var argscheck = require ('cordova/argscheck')
+cordova.define("com.flyingsoftgames.googleplayservices.GooglePlayServices", function(require, exports, module) { var argscheck = require ('cordova/argscheck')
 var exec      = require ('cordova/exec')
 
 module.exports = function () {
  var exports = {}
- 
- exports.getPlayerId = function (init) {
-  var success = (typeof init.success != "undefined") ? init.success : function () {}
-  var error   = (typeof init.error   != "undefined") ? init.error   : function () {}
-  cordova.exec (success, error, "GooglePlayServices", "getPlayerId", [])
- }
  
  exports.initialize = function (init) {
   var success = (typeof init.success != "undefined") ? init.success : function () {}
@@ -16,5 +10,19 @@ module.exports = function () {
   cordova.exec (success, error, "GooglePlayServices", "tryConnect", [])
  }
  
+ exports.getPlayerId = function (init) {
+  var success = (typeof init.success != "undefined") ? init.success : function () {}
+  var error   = (typeof init.error   != "undefined") ? init.error   : function () {}
+  cordova.exec (success, error, "GooglePlayServices", "getPlayerId", [])
+ }
+ 
+ exports.getAccessToken = function (init) {
+  var success = (typeof init.success != "undefined") ? init.success : function () {}
+  var error   = (typeof init.error   != "undefined") ? init.error   : function () {}
+  cordova.exec (success, error, "GooglePlayServices", "getAccessToken", [])
+ }
+ 
  return exports
 } ()
+
+});
