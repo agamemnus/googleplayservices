ab38cf56c6c4368ea4b2bb8cd68487ad14dc0260
agamemnus <agamemnus@gmail.com>
2015-01-02 19:26:27 -0500

    NOPE..

--- www/googleplayservices.js
+++ www/googleplayservices.js
@@ -2,21 +2,18 @@ module.exports = function () {
  var exports = {}
  
  exports.initialize = function (init) {
-  var init = init || {}
   var success = (typeof init.success != "undefined") ? init.success : function () {}
   var error   = (typeof init.error   != "undefined") ? init.error   : function () {}
   cordova.exec (success, error, "GooglePlayServices", "tryConnect", [])
  }
  
  exports.getPlayerId = function (init) {
-  var init = init || {}
   var success = (typeof init.success != "undefined") ? init.success : function () {}
   var error   = (typeof init.error   != "undefined") ? init.error   : function () {}
   cordova.exec (success, error, "GooglePlayServices", "getPlayerId", [])
  }
  
  exports.getAccessToken = function (init) {
-  var init = init || {}
   var success = (typeof init.success != "undefined") ? init.success : function () {}
   var error   = (typeof init.error   != "undefined") ? init.error   : function () {}
   cordova.exec (success, error, "GooglePlayServices", "getAccessToken", [])
